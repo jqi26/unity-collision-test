e9c2261f942f26ed84d164a413debcc44e67101f
Jackie Qiu <jqi26@uclive.ac.nz>
2021-08-26 18:14:33 +1200

    Adding files

---
+++ Assets/TriggerScript.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TriggerScript : MonoBehaviour
+{
+    public Renderer renderer;
+    public Color color;
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        renderer = GetComponent<Renderer>();
+        color = renderer.material.color;
+        color.r = 0;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    void OnTriggerEnter(Collider other)
+    {
+        Debug.Log("Trigger with " + other.gameObject.name);
+        color.r = 1;
+        renderer.material.color = color;
+    }
+}
